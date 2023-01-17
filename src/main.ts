166c6f618e56477bf3769953a8c1e942f2f76925
rookiecol <juan.diegam@hotmail.com>
2023-01-16 20:27:30 -0500

    feat: swagger document init

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -12,6 +12,9 @@ async function bootstrap() {
       forbidNonWhitelisted: true,
     })
   );
+  const options = new DocumentBuilder().setTitle('Sabana Growers Co API').setVersion('1.0.0').build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('docs', app, document);
   await app.listen(3000);
 }
 bootstrap();
